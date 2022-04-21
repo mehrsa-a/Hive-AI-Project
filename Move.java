8930fe56c165dc2d442e43726d50f1740e3f26d6
Amirreza <amir1381raei@gmail.com>
2022-04-21 09:25:16 +0430

    add locust move (incomplete!)

--- Move.java
+++ Move.java
@@ -1,3 +1,96 @@
 public class Move {
-
+    // move for Locust
+    public boolean locustMove(Boarder boarder, Piece piece, String direction) {
+        int new_X = 0;
+        int new_y = 0;
+        switch (direction) {
+            case "w":
+                if (boarder.pieces[piece.x - 2][piece.y] == null) {
+                    return false;
+                } else {
+                    new_X = piece.x - 2;
+                    new_y = piece.y;
+                    while (boarder.pieces[new_X][new_y] == null) {
+                        new_X -= 2;
+                    }
+                    piece.x = new_X;
+                    // change space and free space function todo
+                }
+                break;
+            case "e":
+                if (boarder.pieces[piece.x + 2][piece.y] == null) {
+                    return false;
+                } else {
+                    new_X = piece.x + 2;
+                    new_y = piece.y;
+                    while (boarder.pieces[new_X][new_y] == null) {
+                        new_X += 2;
+                    }
+                    piece.x = new_X;
+                    // change space and free space function todo
+                }
+                break;
+            case "se":
+                if (boarder.pieces[piece.x + 1][piece.y - 1] == null) {
+                    return false;
+                } else {
+                    new_X = piece.x + 1;
+                    new_y = piece.y - 1;
+                    while (boarder.pieces[new_X][new_y] == null) {
+                        new_X += 1;
+                        new_y -= 1;
+                    }
+                    piece.x = new_X;
+                    piece.y = new_y;
+                    // change space and free space function todo
+                }
+                break;
+            case "ne":
+                if (boarder.pieces[piece.x + 1][piece.y + 1] == null) {
+                    return false;
+                } else {
+                    new_X = piece.x + 1;
+                    new_y = piece.y + 1;
+                    while (boarder.pieces[new_X][new_y] == null) {
+                        new_X += 1;
+                        new_y += 1;
+                    }
+                    piece.x = new_X;
+                    piece.y = new_y;
+                    // change space and free space function todo
+                }
+                break;
+            case "sw":
+                if (boarder.pieces[piece.x - 1][piece.y - 1] == null) {
+                    return false;
+                } else {
+                    new_X = piece.x - 1;
+                    new_y = piece.y - 1;
+                    while (boarder.pieces[new_X][new_y] == null) {
+                        new_X -= 1;
+                        new_y -= 1;
+                    }
+                    piece.x = new_X;
+                    piece.y = new_y;
+                    // change space and free space function todo
+                }
+                break;
+            case "nw":
+                if (boarder.pieces[piece.x - 1][piece.y + 1] == null) {
+                    return false;
+                } else {
+                    new_X = piece.x - 1;
+                    new_y = piece.y + 1;
+                    while (boarder.pieces[new_X][new_y] == null) {
+                        new_X -= 1;
+                        new_y += 1;
+                    }
+                    piece.x = new_X;
+                    piece.y = new_y;
+                    // change space and free space function todo
+                }
+                break;
+        }
+        return true;
+    }
 }
